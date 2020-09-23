08fd0a42a86f3d481e9986b7581242ad7d9aa786
Duan Yihan <69476139+Ian-Dx@users.noreply.github.com>
2020-09-23 14:43:11 +0800

    Update

--- SidewalkDetection/src/__init__.py
+++ SidewalkDetection/src/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["main", "processor", "train"]
+__all__ = ["main", "processor", "train", "html_gene"]
---
+++ SidewalkDetection/src/html_gene.py
@@ -0,0 +1,96 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+def html_gene(
+        model, train_img, validation_img, tv_rate, paper_url,
+        paper_name, code_url, mIoU, mDSC, loss, train_time, images
+):
+    """
+    Generate a html output
+    :param model:
+    :param train_img:
+    :param validation_img:
+    :param tv_rate:
+    :param paper_url:
+    :param paper_name:
+    :param code_url:
+    :param mIoU:
+    :param mDSC:
+    :param loss:
+    :param train_time:
+    :param images:
+    :return:
+    """
+    # Set the environment
+    env = Environment(loader=FileSystemLoader("./results/"))
+    # Get template
+    template = env.get_template("train_output_template.html")
+    # Render the template
+    with open("./results/train_output.html", "w+") as out_html:
+        html_content = template.render(
+            model=model,
+            train_img=train_img,
+            validation_img=validation_img,
+            tv_rate=tv_rate,
+            paper_url=paper_url,
+            paper_name=paper_name,
+            code_url=code_url,
+            mIoU=mIoU,
+            mDSC=mDSC,
+            loss=loss,
+            train_time=train_time,
+            images=images
+        )
+        out_html.write(html_content)
+
+
+def image_gene(imageID, image, label, loss, IoU, DSC):
+    """
+    Generate a image row
+    :param imageID:
+    :param image:
+    :param label:
+    :param loss:
+    :param IoU:
+    :param DSC:
+    :return:
+    """
+    return {
+        "imageID": imageID,
+        "image": image,
+        "label": label,
+        "loss": loss,
+        "IoU": IoU,
+        "DSC": DSC
+    }
+
+
+def densASPP_test():
+    images = []
+    image = image_gene(
+        "000001",
+        "../resources/data/aachen_000000_000019_leftImg8bit.png",
+        "../resources/target/aachen_000000_000019_gtFine_color.png",
+        12,
+        "80%",
+        "78%"
+    )
+    images.append(image)
+    html_gene(
+        "DenseASPP",
+        3000,
+        2000,
+        1.5,
+        "https://github.com/Ian-Dx/SidewalkDetection/blob/master/references/Yang_DenseASPP_for_Semantic_CVPR_2018_paper.pdf",
+        "DenseASPP for Semantic Segmentation in Street Scenes",
+        "https://github.com/Ian-Dx/SidewalkDetection/tree/master/SidewalkDetection",
+        "85%",
+        "84%",
+        100,
+        12121,
+        images
+    )
+
+
+if __name__ == '__main__':
+    densASPP_test()
